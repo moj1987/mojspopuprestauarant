13e98433a3a4e7fff805bed606aa42f3ee1ffc06
Rich Hildred <rhildred@gmail.com>
2021-08-17 10:10:32 -0400

    put paypal interface in pwa app

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ module.exports = class extends Page {
         const oJson = fetch("https://prog8110summer2021-default-rtdb.firebaseio.com/meals.json").json();
         console.log(oJson);
         let sResult = "<h1>Upcoming Popup Meals</h1>";
+        let n = 0;
         Object.keys(oJson).map((key) => {
             const oEntity = oJson[key];
             console.log(oEntity);
@@ -17,11 +18,10 @@ module.exports = class extends Page {
             <h2>${oEntity.title}</h2>
             <p><img src="${oEntity.featured_image}" alt="${oEntity.title}"</p>
             <p>${oEntity.full_description}</p>
-            <form action="https://serene-taiga-04277.herokuapp.com/payment" method="post">
-            <input type="hidden" name="title" value="${oEntity.title}" />
-            <input type="hidden" name="price" value="21" />
-            <input type="tel" placeholder="enter your number" name="telephone"/>
-            <button type="submit">Order now</button>
+            <form>
+                <button id="button_${n++}" disabled class="paypal_button" data-cost="12.99">
+                Order now
+                </button>
             </form>
             `;
         });
